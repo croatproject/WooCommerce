66608fa1f620c3cc2376cec6a75d0663354a83e0
jerkitu <37352462+jerkitu@users.noreply.github.com>
2018-08-30 01:43:02 +0200

    New options
    Show / Hide price with CROAT
    Show / Hide QR Code
    Show / Hide Wallet Address
    Set parity source
    Set number of decimals
    
    Added Price to Product Page
    Added line with CROAT Total to Cart Page
    Added line with CROAT Total to Order Page
    
    * Pending Translate files

--- qr_pagament.php
+++ qr_pagament.php
@@ -1,12 +1,14 @@
 <?php
 //include "phpqrcode/qrlib.php";
-include ("phpqrcode.php");
+include ("lib/phpqrcode.php");
 
 $preu = $_POST['preu'];
 
     $crt = "crt111";
 	$wallet = $_GET['wallet']; 
 	$preu = $_GET['preu']; 
+	//$preu = 10; 	
+	//$payment_id = "e6f1c88220a80ea3b254a181678155f18285fedaac6673d6f934e2f91182914c";
      
     // we need to be sure ours script does not output anything!!! 
     // otherwise it will break up PNG binary! 
